bb1af221d444d2c76f5da6c1556e2a015d8afc39
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2021-01-27 23:40:43 -0600

    Updated the locations

---
+++ init-locations.js
@@ -0,0 +1,30 @@
+export default [
+    {
+      Name: 'Library',
+      North: 40.3537,
+      //North: 40.35376855387037,
+      West: -94.8864,
+      //West: -94.88643030694007,
+      South: 40.3533,
+      //South: 40.353355663212,
+      East: -94.8857,
+      //East: -94.88573293260741,
+      Notes: 'Trapezium',
+      Attachments: '',
+      'Quest-Location': 'B.D. Owens Library',
+    },
+    {
+      Name: 'Mooyah',
+      North: 40.3545,
+      //North: 40.35456213683928,
+      West: -94.8887,
+      //West: -94.88876920428974,
+      South: 40.3542,
+      //South: 40.35427265491189,
+      East: -94.8881,
+      //East: -94.8881205711812,
+      Notes: 'Time to Eat',
+      Attachments: '',
+      'Quest-Location': 'Mooyah Burgers',
+    }
+];
