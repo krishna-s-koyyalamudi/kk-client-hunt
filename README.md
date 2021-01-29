29e7d9abf77979693f9cd35b2e4e5616ac1fc835
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2021-01-29 09:30:56 -0600

    Add Geo-Location

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ A client-side app with GitHub Pages
 - Source code : https://github.com/Krishna-Koyyalamudi/kk-client-hunt/
 
 - Running app : https://krishna-koyyalamudi.github.io/kk-client-hunt/
+
+## Resources
+
+- ![Location Handler](https://github.com/denisecase/cp-client-hunt)
+- ![Distance Calculation](https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula)
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,10 @@
-    // first imports.......................
+// first imports.......................
 import locationsArray from '../init-locations.js';
 
 // helper functions....................
 
 
 // event handlers......................
-
 
 let locationElement = document.getElementById("location");
 
@@ -49,9 +48,9 @@ async function locationHandler() {
 
     // In case of any error where if the device is not 30m range it displays error.
 
-    if(error) {
+    if (error) {
         document.getElementById("error-message").innerHTML = "You're not in the specified area";
-    } 
+    }
 }
 
 
@@ -59,28 +58,22 @@ async function locationHandler() {
 
 
 function isInside(questLat, questLon) {
-    let distance = distanceBetweenLocations(questLat, questLon);
+    let distance = distanceBetweenLocations(currentlat, currentlon, questLat, questLon);
     console.log("distance: " + distance);
-    if (distance < 100) {
+    if (distance < 30) {
         return true;
     } else {
         return false;
     }
 }
 
-//distance between the lat-long points.
-function distanceBetweenLocations(questLat, questLon) {
-    const R = 6371e3;
-    const φ1 = currentlat * Math.PI / 180;
-    const φ2 = questLat * Math.PI / 180;
-    const Δφ = (questLat - currentlat) * Math.PI / 180;
-    const Δλ = (questLon - currentlon) * Math.PI / 180;
 
-    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-        Math.cos(φ1) * Math.cos(φ2) *
-        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    const d = R * c;
-    return d; 
+function distanceBetweenLocations(currentlat, currentlon, questLat, questLon) {
+    var p = 0.017453292519943295;
+    var a = 0.5 - Math.cos((questLat - currentlat) * p) / 2 +
+        Math.cos(currentlat * p) * Math.cos(questLat * p) *
+        (1 - Math.cos((questLon - currentlon) * p)) / 2;
+    return 12742 * Math.asin(Math.sqrt(a));
 }
+
+
