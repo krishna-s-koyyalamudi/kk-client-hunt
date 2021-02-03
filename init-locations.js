2d1667cec699f3d4cce811794225f0620efb776e
Krishna-Koyyalamudi <krishna.kks3797@gmail.com>
2021-02-02 21:35:32 -0600

    locations fix

--- init-locations.js
+++ init-locations.js
@@ -5,17 +5,17 @@ export default [
       Longitude: -94.879261, 
       // 'Quest-Location': 'Horizons West Apartments',
     },
-    {
-      Name: 'Colden Hall',
-      //40.35085725998568, -94.88238755790566
-      Latitude:40.35085725998568,
-      Longitude:-94.88238755790566,
-      // North:40.35085725998568,
-      // South:40.35085725998568,
-      // East:-94.88238755790566,
-      // West:-94.88238755790566,
-      // 'Quest-Location': 'Colden Hall',
-    },
+    // {
+    //   Name: 'Colden Hall',
+    //   //40.35085725998568, -94.88238755790566
+    //   Latitude:40.35085725998568,
+    //   Longitude:-94.88238755790566,
+    //   // North:40.35085725998568,
+    //   // South:40.35085725998568,
+    //   // East:-94.88238755790566,
+    //   // West:-94.88238755790566,
+    //   // 'Quest-Location': 'Colden Hall',
+    // },
     {
       Name: 'Foster Fitness Center',
     //   //40.35058771997885, -94.88397573885968
